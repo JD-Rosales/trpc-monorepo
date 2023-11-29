5ed791f0498e9785ff2e63f211ed459902268f19
JD-Rosales <rosales.jake1999@gmail.com>
2023-11-29 22:24:09 +0800

    Setup trpc both server and client

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,12 +1,22 @@
 import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { appRouter } from './router';
 
 const PORT: number = Number(process.env.PORT) || 8000;
 
 const app: Application = express();
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.json({ message: 'Hello world!' });
-});
+// app.get('/', (req: Request, res: Response, next: NextFunction) => {
+//   res.json({ message: 'Hello world!' });
+// });
+app.use(cors());
+app.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Server running on Port: ${PORT}`);
